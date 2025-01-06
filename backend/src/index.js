f4063a3f4e90bd2084d0d417654847dabb7ec241
Juyoung <ljy9407@gmail.com>
2025-01-06 00:57:03 -0700

    Fix: cors error to deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      "https://hackathon-web-lime.vercel.app/planResult?id=i4AMeGIHmRpyFYrREjVy",
-      "http://localhost:3000",
-    ],
+    origin: ["https://hackathon-web-lime.vercel.app", "http://localhost:3000"],
     methods: ["GET", "POST"],
     credentials: true,
   })
